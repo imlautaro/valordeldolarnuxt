e8373328038529775544510ebea5d85d0c7535b9
Lautaro Pereyra <hi@imlautaro.com>
2022-12-19 12:23:12 -0300

    add title

--- src/app.vue
+++ src/app.vue
@@ -1,9 +1,18 @@
 <script setup>
 const data = await useDolarSi();
+
+useHead({
+	title: 'Valor del Dólar Nuxt',
+});
 </script>
 
 <template>
 	<div>
+		<div
+			class="p-4 bg-primary-600 text-white flex items-center justify-center text-center shadow"
+		>
+			<span class="text-xl font-bold">Valor del Dólar Nuxt</span>
+		</div>
 		<div class="container max-w-sm py-12 space-y-6 mx-auto px-4">
 			<PriceCard
 				v-for="item in data"
